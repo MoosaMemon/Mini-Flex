5d2302c673a7e60d24c402c93f5f636b582b1433
Moosa Memon <officialmoosamemon@gmail.com>
2023-05-01 02:01:07 +0500

    Update Test.java
    trying to update proj on github

--- src/Test.java
+++ src/Test.java
@@ -3,5 +3,7 @@ public class Test
     public static void main(String[] args) {
         System.out.println("just testing boi");
         System.out.println("hahah");
+        System.out.println("This change is made through github");
+        
     }
 }
