6a4a7a910a3feb3f53029bf5c790d7c95803661b
Moosa Memon <officialmoosamemon@gmail.com>
2023-05-01 02:03:28 +0500

    deleted the Test.java file from project

--- src/Test.java
+++
@@ -1,9 +0,0 @@
-public class Test
-{
-    public static void main(String[] args) {
-        System.out.println("just testing boi");
-        System.out.println("hahah");
-        System.out.println("This change is made through github");
-        
-    }
-}
